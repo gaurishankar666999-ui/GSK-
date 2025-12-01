1d71fddda7cf4085cfed298f868532bfd5fd9456
gaurishankar666999-ui <gaurishankar666999@gmail.com>
2025-12-01 13:03:41 +0530

    Update play.py

--- IstkharMusic/utils/inline/play.py
+++ IstkharMusic/utils/inline/play.py
@@ -65,10 +65,10 @@ def stream_markup_timer(_, chat_id, played, dur):
         ],
       [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_ISTKHAR_BABY_lll",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/gaurishankarsingh666",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/iamIstkhar",
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/GSKCHANNEL666",
             )
         ],
          [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -87,10 +87,10 @@ def stream_markup(_, chat_id):
          ],
         [
             InlineKeyboardButton(
-                text="ᴏᴡɴᴇʀ", url="https://t.me/ll_istkhar_baby_lll",
+                text="ᴏᴡɴᴇʀ", url="https://t.me/gaurishankarsingh666",
             ),
             InlineKeyboardButton(
-                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/+rHfbX06JYoNlNDg9",
+                text="sᴜᴩᴩᴏʀᴛ", url="https://t.me/GSKCHANNEL666",
             )
         ],
         [InlineKeyboardButton(text=_["CLOSE_BUTTON"], callback_data="close")],
@@ -168,3 +168,4 @@ def slider_markup(_, videoid, user_id, query, query_type, channel, fplay):
     ]
     return buttons
     
+
